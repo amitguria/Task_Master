1f65bb843b636662c73a85795b6b941d47ffa376
Amit Guria <amitguria2022@gmail.com>
2023-09-29 12:11:13 +0530

    Update Boolean in Todo.js

--- Server/Models/Todo.js
+++ Server/Models/Todo.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose')
 const TodoSchema = new mongoose.Schema({
     title: String,
     content: String
+    done : {
+        type: Boolean,
+        default: false
+    }
 })
 
 const TodoModel = mongoose.model("todos", TodoSchema) //todos is the name of our database
